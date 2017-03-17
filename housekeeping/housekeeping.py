8d2dbb26f6a4f6e3079738a336849e127cf31358
Sebastian Blasiak <sebastian.blasiak@zoomint.com>
2017-03-17 17:48:08 +0100

    logging moved to independent module

--- housekeeping/housekeeping.py
+++ housekeeping/housekeeping.py
@@ -1,14 +1,16 @@
 import argparse
-import logging
 import os
 import shutil
 import sys
 import time
 from subprocess import call
+from blasebast.smoothLogging import smoothLogging
 
-import yaml
+lobject = smoothLogging()
+log = lobject.log("c:/temp", "mylog")
 
 yamlFound = None
+
 print("current script: %s" % (__file__))
 for file in os.listdir(os.path.dirname(__file__)):
     if file.endswith(".yaml"):
@@ -40,21 +42,6 @@ else:
 if not os.path.exists(logPath):
     os.makedirs(logPath)
 
-
-# LOGGING
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)-16s %(levelname)-8s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    filename=os.path.join(logPath,'%s_%s.log') % (os.path.basename(__file__), time.strftime('%Y%m%d%H%M%S')),
-                    filemode='w')
-log_file_name = os.path.normpath(os.path.join(logPath,'%s_%s.log') % (os.path.basename(__file__), time.strftime('%Y%m%d%H%M%S')))
-logger = logging.StreamHandler()
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)-16s %(levelname)-8s %(message)s')
-logger.setFormatter(formatter)
-logging.getLogger('').addHandler(logger)
-# END LOGGING
-
 # VARIABLES
 if os.path.exists("config.yaml"):
     yamlFile = "config.yaml"
@@ -76,7 +63,7 @@ def check_age(path):
         ageInDays = round((time.time() - ctime)/(3600*24))
         return(ageInDays)
     except:
-        logging.error("cannot stat the file '%s', access denied?" % path)
+        log.error("cannot stat the file '%s', access denied?" % path)
         return 0
 
 def delete_files_by_age(extension, age, path, move_only, *args):
@@ -92,13 +79,13 @@ def delete_files_by_age(extension, age, path, move_only, *args):
                             target = os.path.join(root,dir)
                             shutil.rmtree(target)
                             if move_only is ".":
-                                logging.info("deleting %s, age is: %s days" % (os.path.join(path,dir),itsAge))
+                                log.info("deleting %s, age is: %s days" % (os.path.join(path,dir),itsAge))
                                 shutil.rmtree(target)
                             else:
                                 if not os.path.exists(move_only):
                                     try: os.makedirs(path)
-                                    except: logging.error("cannot create path: %s" % (move_only))
-                                logging.info("moving %s, age is: %s days [dest: %s]" % (os.path.join(path,dir),itsAge,move_only))
+                                    except: log.error("cannot create path: %s" % (move_only))
+                                log.info("moving %s, age is: %s days [dest: %s]" % (os.path.join(path,dir),itsAge,move_only))
                                 shutil.move(target,os.path.normpath(move_only))
                         except:
                             raise
@@ -112,22 +99,22 @@ def delete_files_by_age(extension, age, path, move_only, *args):
                         try:
                             itsAge = check_age(filepath)
                             if move_only is ".":
-                                logging.info("removing %s, age is: %s days" % (filepath,itsAge))
+                                log.info("removing %s, age is: %s days" % (filepath,itsAge))
                                 os.unlink(filepath)
                             else:
                                 if not os.path.exists(move_only):
                                     try: os.makedirs(path)
-                                    except: logging.error("cannot create path: %s" % (move_only))
-                                logging.info("moving %s, age is: %s days [dest: %s]" % (filepath,itsAge, move_only))
+                                    except: log.error("cannot create path: %s" % (move_only))
+                                log.info("moving %s, age is: %s days [dest: %s]" % (filepath,itsAge, move_only))
                                 shutil.move(filepath, move_only)
                         except:
-                            logging.error("cannot move / delete %s, but proceeding with rest" % filepath)
+                            log.error("cannot move / delete %s, but proceeding with rest" % filepath)
 
 def create_scheduled_task(recycleWeekDay, recycleWeekDayTime, executableFile, taskName):
     removeScheduledTaskCmd = "SCHTASKS /Delete /TN %s /F" % (taskName)
     addScheduledTaskCmd = "SCHTASKS /Create /NP /SC weekly /D %s /TN %s /ST %s /TR \"%s\"" % (recycleWeekDay,taskName,recycleWeekDayTime,python + " " + executableFile + " -createTask no -logPath %s") % (logPath)
-    logging.info("command for removing scheduled task is: %s" % removeScheduledTaskCmd)
-    logging.info("command for adding scheduled task is: %s" % addScheduledTaskCmd)
+    log.info("command for removing scheduled task is: %s" % removeScheduledTaskCmd)
+    log.info("command for adding scheduled task is: %s" % addScheduledTaskCmd)
 
     call(removeScheduledTaskCmd,shell=True)
     call(addScheduledTaskCmd,shell=True)
@@ -140,13 +127,13 @@ try:
             age = settings['age']
             move_only = os.path.normpath(settings['move_only'])
         except:
-            logging.error("yaml structure is not as expected, it should contain path, extension and age in it's structure" % (yamlFile))
+            log.error("yaml structure is not as expected, it should contain path, extension and age in it's structure" % (yamlFile))
         for extension in extensions.split(','):
             delete_files_by_age(extension,age,path, move_only)
             try:
                 delete_files_by_age(extension,age,path, move_only)
             except:
-                logging.error("cannot delete extension %s on %s" % (extension,path))
+                log.error("cannot delete extension %s on %s" % (extension,path))
         try:
             dirpatterns = settings['dirpatterns']
             for dirpattern in dirpatterns.split(','):
@@ -162,16 +149,16 @@ if args.createTask == "yes" and os.path.exists(os.path.normpath(python)):
         taskName = yamlStream['removeOldFilesSchedule']['taskname']
         create_scheduled_task(recycleWeekDay,recycleWeekDayTime,executableFile,taskName)
     except:
-        logging.warn("incorrectly enabled task?")
+        log.warn("incorrectly enabled task?")
     try:
         src = os.path.join(os.path.dirname(__file__),"yaml")
         dst = os.path.join("yaml")
         if not os.path.exists(dst): os.makedirs(dst)
         if not os.path.exists(dst):
             shutil.copytree(src,dst)
-            logging.info("copied yaml lib to %s" % dst)
+            log.info("copied yaml lib to %s" % dst)
     except:
-        logging.error("cannot copy %s to %s" % (src,dst))
+        log.error("cannot copy %s to %s" % (src,dst))
 else:
     if not os.path.exists(python):
-        logging.error("%s doesn't exist" % python)
+        log.error("%s doesn't exist" % python)
